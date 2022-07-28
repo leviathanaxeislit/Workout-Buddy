a6823a681a3f413d353aa559b96ef2b7d14dafd2
Chandra Prakash <scp.thedevil@gmail.com>
2022-07-28 14:58:44 +0530

    Auth Forms and Routes
    Signed-off-by: Chandra Prakash <scp.thedevil@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 //pages and components
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 function App() {
@@ -12,6 +14,12 @@ function App() {
   <Routes>
     <Route path='/'
      element={<Home />}
+     />
+     <Route path='/login'
+     element={<Login />}
+     />
+     <Route path='/signup'
+     element={<Signup />}
      />
   </Routes>
 </div>
